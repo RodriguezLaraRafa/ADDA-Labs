8958e51b10868a0471b73810eeca32ccfd1ba1ca
Rafael Rodríguez <rodriguezlara.rafa@gmail.com>
2025-03-14 14:50:55 +0100

    GA for Exercise 3 completed

---
+++ lab3/src/ejercicio3/Ejercicio3AG.java
@@ -0,0 +1,94 @@
+package ejercicio3;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import us.lsi.ag.ValuesInRangeData;
+import us.lsi.ag.agchromosomes.ChromosomeFactory.ChromosomeType;
+
+
+//Voy a representar el cromosoma como un array bidimensional [tipo, area] aplanado
+
+public class Ejercicio3AG implements ValuesInRangeData<Integer, SolucionFestival>{
+	public Ejercicio3AG(String file) {
+		DatosFestival.iniDatos(file);
+
+	}
+
+	@Override
+	public Integer size() {
+		// TODO Auto-generated method stub
+		return DatosFestival.getNumAreas() * DatosFestival.getNumTiposEntrada();
+	}
+
+	@Override
+	public ChromosomeType type() {
+		// TODO Auto-generated method stub
+		return ChromosomeType.Range;
+	}
+
+	@Override
+	public Double fitnessFunction(List<Integer> ls) {
+		// TODO Auto-generated method stub
+		Double goal = 0.0;
+		Double penalties = 0.0;
+		Map<Integer, Integer> aforoPorArea = new HashMap<Integer, Integer>();
+		Map<Integer, Integer> aforoPorTipo = new HashMap<Integer, Integer>();
+
+		
+		for(int i=0; i<ls.size();i++) {
+			Integer aforoAreaTipo = ls.get(i);
+			Integer currentType = i / DatosFestival.getNumTiposEntrada();
+			Integer currentArea = i % DatosFestival.getNumAreas();
+			goal+= aforoAreaTipo * DatosFestival.getCosteAsignacion(currentType, currentArea);
+			
+			if(aforoPorArea.containsKey(currentArea)) {
+				aforoPorArea.put(currentArea, aforoPorArea.get(currentArea) + aforoAreaTipo);
+			} else {
+				aforoPorArea.put(currentArea, aforoAreaTipo);
+			}
+			if(aforoPorTipo.containsKey(currentType)) {
+				aforoPorTipo.put(currentType, aforoPorTipo.get(currentType) + aforoAreaTipo);
+			} else {
+				aforoPorTipo.put(currentType, aforoAreaTipo);
+			}
+			
+			
+		}
+		
+		for(Integer key: aforoPorArea.keySet()) {
+			if(DatosFestival.getAforoMaximoArea(key) < aforoPorArea.get(key)) {
+				penalties += (aforoPorArea.get(key) - DatosFestival.getAforoMaximoArea(key));
+			}
+		}
+		
+		for(Integer key: aforoPorTipo.keySet()) {
+			if(DatosFestival.getCuotaMinima(key) > aforoPorTipo.get(key)) {
+				penalties += (DatosFestival.getCuotaMinima(key) - aforoPorTipo.get(key))*20; //Cambiad el valor de la constante
+			}
+		}
+		
+		
+		return -goal- 10000*penalties;  //Cambiad el valor de la constante
+	}
+
+	@Override
+	public SolucionFestival solucion(List<Integer> value) {
+		// TODO Auto-generated method stub
+		return SolucionFestival.create(value);
+	}
+
+	@Override
+	public Integer max(Integer i) {
+		// TODO Auto-generated method stub
+		return DatosFestival.getAforoMaximoArea(i % DatosFestival.getNumAreas());
+	}
+
+	@Override
+	public Integer min(Integer i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ lab3/src/ejercicio3/SolucionFestival.java
@@ -0,0 +1,118 @@
+package ejercicio3;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import ejercicio2.Ejercicio2AG;
+import ejercicio2.SolucionCursos;
+import us.lsi.ag.agchromosomes.AlgoritmoAG;
+import us.lsi.ag.agstopping.StoppingConditionFactory;
+
+import java.util.HashMap;
+
+public class SolucionFestival {
+
+    public static SolucionFestival create(List<Integer> ls) {
+        return new SolucionFestival(ls);
+    }
+
+    private int numAsignaciones;
+    private Map<Integer, Integer> solucion;
+    private Double costeTotal;
+    private int unidadesTotales;
+
+    private SolucionFestival(List<Integer> ls) {
+    	numAsignaciones = ls.size();
+    	solucion = new HashMap<>();
+    	costeTotal = 0.0;
+    	unidadesTotales = 0;
+    	
+    	for(int i =0;i<ls.size();i++) {
+			Integer aforoAreaTipo = ls.get(i);
+			Integer currentType = i / DatosFestival.getNumTiposEntrada();
+			Integer currentArea = i % DatosFestival.getNumAreas();
+    		costeTotal += DatosFestival.getCosteAsignacion(currentType, currentArea);
+    		unidadesTotales += aforoAreaTipo;
+    		solucion.put(i, aforoAreaTipo);
+    	}
+    	
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Resumen de asignaciones:\n");
+
+        Map<Integer, Integer> aforoOcupadoPorArea = new HashMap<>();
+        Map<Integer, Map<Integer, Integer>> entradasPorArea = new HashMap<>();
+
+        for (Map.Entry<Integer, Integer> entry : solucion.entrySet()) {
+            Integer tipoEntrada = entry.getKey() / DatosFestival.getNumAreas();
+            Integer area = entry.getKey() % DatosFestival.getNumAreas();
+            Integer unidades = entry.getValue();
+
+            if (unidades > 0) {
+                aforoOcupadoPorArea.put(area, aforoOcupadoPorArea.getOrDefault(area, 0) + unidades);
+                entradasPorArea.computeIfAbsent(area, k -> new HashMap<>())
+                        .put(tipoEntrada, entradasPorArea.get(area).getOrDefault(tipoEntrada, 0) + unidades);
+            }
+        }
+
+        for (int i = 0; i < DatosFestival.getNumAreas(); i++) {
+            Integer aforoOcupado = aforoOcupadoPorArea.getOrDefault(i, 0);
+            if (aforoOcupado > 0) {
+                result.append(String.format("Aforo área %s: %d/%d\n",
+                        DatosFestival.getArea(i).nombre(),
+                        aforoOcupado,
+                        DatosFestival.getAforoMaximoArea(i)));
+
+                entradasPorArea.getOrDefault(i, new HashMap<>()).forEach((tipoEntrada, unidades) ->
+                        result.append(String.format("Tipo de entrada %s asignadas: %d unidades\n",
+                                DatosFestival.getTipoEntrada(tipoEntrada).tipo(), unidades)
+                ));
+            }
+        }
+
+        result.append(String.format("\nCoste total: %.2f\nUnidades totales: %d\n", costeTotal, unidadesTotales));
+
+        return result.toString();
+    }
+
+    public Integer getNumAsignaciones() {
+        return numAsignaciones;
+    }
+
+    public Map<Integer, Integer> getSolucion() {
+        return solucion;
+    }
+
+    public Double getCosteTotal() {
+        return costeTotal;
+    }
+
+    public Integer getUnidadesTotales() {
+        return unidadesTotales;
+    }
+    
+    public static void main(String[] args) {
+		Locale.setDefault(Locale.of("en", "US"));
+
+		AlgoritmoAG.ELITISM_RATE = 0.10;
+		AlgoritmoAG.CROSSOVER_RATE = 0.95;
+		AlgoritmoAG.MUTATION_RATE = 0.8;
+		AlgoritmoAG.POPULATION_SIZE = 1000;
+
+		StoppingConditionFactory.NUM_GENERATIONS = 10000;
+		StoppingConditionFactory.MAX_ELAPSEDTIME = 50;
+		StoppingConditionFactory.stoppingConditionType = StoppingConditionFactory.StoppingConditionType.GenerationCount;
+
+		Ejercicio3AG p = new Ejercicio3AG("resources/ejercicio3/DatosEntrada1.txt");
+
+		AlgoritmoAG<List<Integer>, SolucionFestival> ap = AlgoritmoAG.of(p);
+		ap.ejecuta();
+
+		System.out.println("================================");
+		System.out.println(ap.bestSolution());
+		System.out.println("================================");
+	}
+}
